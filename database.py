f1033a63750386037b3b00f9b834243f3d5671c5
AVTAR SINGH <130889026+Avtarsingh97@users.noreply.github.com>
2024-05-07 09:40:08 +0530

    Add files via upload

---
+++ database.py
@@ -0,0 +1,32 @@
+import mysql.connector
+
+
+class Database:
+    def __init__(self, config):
+        self.config = config
+        self.connection = None
+
+    def __enter__(self):
+        self.connection = mysql.connector.connect(**self.config)
+        return self.connection
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.connection:
+            self.connection.close()
+
+def connect_db():
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="1234",
+            database="bank"
+        )
+        
+        return connection
+    except mysql.connector.Error as err:
+        print("Failed to connect to database:", err)
+        return None
+        
+# db=connect_db()
+# cursor = db.cursor() 
